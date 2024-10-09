ddc5ad5d168114107e68ad510f175d2e8108de36
Leo Mensah <83732595+leomensah@users.noreply.github.com>
2024-10-09 21:57:55 +0000

    modified ascii scripts

--- ascii-script.sh
+++ ascii-script.sh
@@ -3,3 +3,6 @@ sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
+
+
+# adding a comment to the ascii script
